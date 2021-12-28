7e4c9139be66f000fcc1069c2e3b1ff6484ad32c
DeboBurro <debo@burro.ai>
2021-12-28 16:55:49 -0500

    create global variable

--- src/main/helper.go
+++ src/main/helper.go
@@ -1,9 +1,9 @@
 package notmain
 // this file will be considered as notmain package but under main folder so we need to import "go-test/main" to import this pkg
 
-
 import "strings"
 
+var MyGlobalVar = "global variable"
 // Need to specify an array of output datatypes
 // Capitalize the function means it will exported as a sort of public function that other package can use
 func ValidateUserInput(firstName string, lastName string, email string) (bool, bool){
